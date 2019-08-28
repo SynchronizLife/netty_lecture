00a4334a0b1b87d9c1c83dcab448e3570fc4cbd6
yuwei <15868833146@163.com>
2019-08-29 07:43:16 +0800

    netty实现多客户端连接与通讯

---
+++ src/main/java/com/yuwei/netty/thirdexample/MyChatServerHandler.java
@@ -0,0 +1,62 @@
+package com.yuwei.netty.thirdexample;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+public class MyChatServerHandler extends SimpleChannelInboundHandler<String> {
+
+    private static ChannelGroup channelGroup = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        Channel channel = ctx.channel();
+
+        channelGroup.forEach(ch ->{
+            if(channel != ch){
+                ch.writeAndFlush(ch.remoteAddress() + " 发送的消息：" + msg + "\n");
+            }else {
+                ch.writeAndFlush("【自己】 " + msg + "\n");
+            }
+        });
+    }
+
+
+    //客户端连接上
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+
+        channelGroup.writeAndFlush("【服务器】 - " + channel.remoteAddress() + " 加入\n");
+        channelGroup.add(channel);
+    }
+
+    //客户端连接断开:无需手动的remove channel，会自动的移除channel
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+        channelGroup.writeAndFlush("【服务器】- " + channel.remoteAddress() + " 断开\n");
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+        System.out.println(channel.remoteAddress() + " 上线");
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+        System.out.println(channel.remoteAddress() + " 下线");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+
+    }
+
+
+}
