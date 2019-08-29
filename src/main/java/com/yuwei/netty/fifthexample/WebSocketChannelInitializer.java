5c5609a87ab303fb1bd0efb4a5e531ce9457a112
yuwei <15868833146@163.com>
2019-08-30 00:43:44 +0800

    update test

--- src/main/java/com/yuwei/netty/fifthexample/WebSocketChannelInitializer.java
+++ src/main/java/com/yuwei/netty/fifthexample/WebSocketChannelInitializer.java
@@ -21,7 +21,7 @@ public class WebSocketChannelInitializer extends ChannelInitializer<SocketChanne
 
         pipeline.addLast(new HttpServerCodec());
         pipeline.addLast(new ChunkedWriteHandler());
-        //netty对于请求采取了分段的方式，下面的处 理器就是将 分段的请求聚合到一起作为一个完整的http请求或是一个完整的http响应
+        //netty对于请求采取了分段  的方式，下面的处 理器就是将 分段的请求聚合到一起作为一个完整的http请求或是一个完整的http响应
         pipeline.addLast(new HttpObjectAggregator(8192));
         pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
         pipeline.addLast(new TextWebSocketFrameHandler());
